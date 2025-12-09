1279c1eb8fb5ada49a59c340afefd6113a96abaf
Leo Player <leo.player@student.unsw.edu.au>
2025-12-10 00:38:54 +1100

    started swift app

---
+++ Email Database Viewer Backend/Email Database Viewer Backend/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Email Database Viewer Backend
+//
+//  Created by プレイヤー怜央 on 2025/12/10.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Email Database Viewer UI/Email Database Viewer UI/Email_Database_Viewer_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Email_Database_Viewer_UIApp.swift
+//  Email Database Viewer UI
+//
+//  Created by プレイヤー怜央 on 2025/12/10.
+//
+
+import SwiftUI
+
+@main
+struct Email_Database_Viewer_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
