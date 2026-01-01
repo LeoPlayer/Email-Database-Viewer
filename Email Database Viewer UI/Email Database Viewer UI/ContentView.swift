a4e7c7fab4d497aa01a3a5a9c8d48ca8a86afee9
Leo Player <leo.player@student.unsw.edu.au>
2026-01-02 02:13:56 +1100

    created new sidebar list

--- Email Database Viewer UI/Email Database Viewer UI/ContentView.swift
+++ Email Database Viewer UI/Email Database Viewer UI/ContentView.swift
@@ -8,12 +8,62 @@
 import SwiftUI
 import Email_Database_Viewer_Backend
 
+// TEMP
+
+enum InboxNames: String, CaseIterable, Identifiable, Hashable {
+    case allEmails = "All Emails"
+    case inbox = "Inbox"
+    case sent = "Sent"
+    case flagged = "Flagged"
+    case draft = "Draft"
+    case deleted = "Deleted"
+    
+    var id: String { rawValue }
+    
+    var systemIcon: String {
+        switch self {
+        case .allEmails:
+            return "tray.fill"
+        case .inbox:
+            return "envelope.fill"
+        case .sent:
+            return "paperplane.fill"
+        case .flagged:
+            return "flag.fill"
+        case .draft:
+            return "pencil.and.scribble"
+        case .deleted:
+            return "trash.fill"
+        }
+    }
+}
+
+protocol sidebarItem: Hashable, Identifiable {
+    var id: String { get }
+    var title: String { get }
+    var systemIcon: String { get }
+    var inboxType: InboxNames { get }
+}
+
+extension sidebarItem {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+// END TEMP
+
 struct ContentView: View {
-    @State private var curStatus: Status = .loaded
+    @State private var splitViewVisibility: NavigationSplitViewVisibility = .all // shows sidebar by default
+    
+    @State private var curStatus: Status = .loaded // stores whether there is an ongoing update to the database
     @State private var searchText: String = ""
-    @State private var previousSearchText = ""
+    @State private var previousSearchText = "" // to determine behaviour when searching the database fails
     @State private var searchResults: [EmailItem] = []
-    @State private var activeError: DatabaseError? = nil
+    @State private var activeError: DatabaseError? // shows alert when not nil
         
     private let backendAPI = BackendAPI()
     private let userDateFormatter: DateFormatter = {
@@ -24,39 +74,106 @@ struct ContentView: View {
         return formatter
     }()
     
-    var body: some View {
-        NavigationView {
-            // sidebar
-            List {
-                Button {
-                    print("temp sidebar button pressed")
-                } label: {
-                    HStack(alignment: .center) {
-                        Image(systemName: "exclamationmark.triangle")
-                            .font(.body)
-                        Text("Temp Button")
-                            .font(.body)
-                    }
-                    .padding(5)
+    
+    // TEMP
+    
+    struct inboxType: sidebarItem {
+        let inboxType: InboxNames
+        let parent: account
+        var title: String { inboxType.rawValue }
+        var systemIcon: String { inboxType.systemIcon }
+        var id: String { parent.title + "." + title }
+    }
+    
+    struct account: sidebarItem {
+        var inboxType: InboxNames { .allEmails }
+        let title: String
+        let systemIcon: String = "person.crop.circle.fill"
+        var id: String {title}
+    }
+    
+    @State private var sidebarSelection: AnyHashable?
+    @State private var expandedStates: [String: Bool] = [:]
+    
+    private var allSidebarAccounts: [account] {
+        return [
+            account(title: "test@gmail.com")
+        ]
+    }
+    
+    private var allSidebarInboxes: [inboxType] {
+        return [
+            inboxType(inboxType: .allEmails, parent: allSidebarAccounts[0]),
+            inboxType(inboxType: .inbox, parent: allSidebarAccounts[0]),
+            inboxType(inboxType: .sent, parent: allSidebarAccounts[0]),
+            inboxType(inboxType: .flagged, parent: allSidebarAccounts[0]),
+            inboxType(inboxType: .draft, parent: allSidebarAccounts[0]),
+            inboxType(inboxType: .deleted, parent: allSidebarAccounts[0])
+        ]
+    }
+    
+    // END TEMP
+    
+    struct InboxTypeView: View {
+        let inboxType: inboxType
+        
+        var body: some View {
+            HStack {
+                Image(systemName: inboxType.systemIcon)
+                    .foregroundStyle(.secondary)
+                Text(inboxType.title)
+            }
+            .padding(.vertical, 4)
+            .contentShape(Rectangle())
+            .tag(inboxType)
+        }
+    }
+    
+    struct AccountView: View {
+        let account: account
+        @Binding var expanded: Bool
+        @Binding var sidebarSelection: AnyHashable?
+        
+        var body: some View {
+            Button(action: {
+                expanded.toggle()
+                sidebarSelection = account
+            }) {
+                HStack {
+                    Image(systemName: account.systemIcon)
+                        .foregroundStyle(sidebarSelection  == AnyHashable(account) ? Color.accentColor : .secondary)
+                    Text(account.title)
                 }
-                .buttonStyle(.automatic)
+                .contentShape(Rectangle())
+            }
+            .buttonStyle(.plain)
+            .tag(account)
+        }
+    }
+    
+    var body: some View {
+        NavigationSplitView(columnVisibility: $splitViewVisibility) {
+            // sidebar
+            List(selection: $sidebarSelection) {
+                ForEach(allSidebarAccounts) { account in
+                    DisclosureGroup(isExpanded: Binding(
+                        get: { expandedStates[account.id, default: true] },
+                        set: { expandedStates[account.id] = $0}
+                    )) {
+                        ForEach(allSidebarInboxes) { inboxItem in
+                            InboxTypeView(inboxType: inboxItem)
+                        }
+                    } label: {
+                        AccountView(account: account, expanded: Binding(
+                            get: { expandedStates[account.id, default: true] },
+                            set: { expandedStates[account.id] = $0}
+                        ), sidebarSelection: $sidebarSelection)
+                    }
+                }
             }
             .listStyle(.sidebar)
             .frame(minWidth: 155)
-            .toolbar {
-                ToolbarItem() {
-                    Button {
-                        print("plus button pressed")
-                        Task {
-                            await addDataToDatabase()
-                        }
-                    } label: {
-                        Image(systemName: "plus")
-                    }
-                }
-            }
-            
-            // main view
+        } content: {
             VStack {
                 VStack(alignment: .leading) {
                     Text("Search Database")
@@ -91,24 +208,17 @@ struct ContentView: View {
                 }
                 
                 if !searchResults.isEmpty {
-                    ScrollViewReader { proxy in
-                        GlassEffectContainer {
-                            List(searchResults) { result in
-                                SearchResult(result: result, dateFormatter: userDateFormatter)
-                            }
-                            .onChange(of: searchResults) {
-                                if searchResults.count > 0 {
-                                    proxy.scrollTo(searchResults[0].id, anchor: UnitPoint.top)
-                                }
-                            }
-                        }
-                    }
+                    SearchResult(results: searchResults, dateFormatter: userDateFormatter)
                 }
                 
                 Spacer()
             }
             .padding()
             .navigationTitle("Email Database Viewer")
+        } detail: {
+            Text("No Email Selected")
+                .font(.title)
+                .foregroundStyle(.tertiary)
         }
         .alert(item: $activeError) { error in
             Alert(
@@ -116,6 +226,16 @@ struct ContentView: View {
                 message: Text(error.message),
                 dismissButton: .default(Text("OK")) {}
             )
+        }
+        .toolbar {
+            ToolbarItem(placement: .automatic) {
+                Button ("Add Database File", systemImage: "plus") {
+                    print("plus button pressed")
+                    Task {
+                        await addDataToDatabase()
+                    }
+                }
+            }
         }
     }
     
